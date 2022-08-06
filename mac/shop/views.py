160937a622298f59bab87d6dae1e80d636edee04
shri142 <shrikrishna2002umbare@gmail.com>
2022-08-06 19:13:17 +0530

    Updated views.py

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -6,4 +6,26 @@ from django.http import HttpResponse
 def index(request):
     return render(request ,'shop/index.html')
 
+def about(request):
+    return HttpResponse("we are at about")
+
+def contact(request):
+    return HttpResponse("we are at contact")
+
+def tracker(request):
+    return HttpResponse("we are at tracker")
+
+def search(request):
+    return HttpResponse("we are at search")
+
+def productView(request):
+    return HttpResponse("we are at productView")
+
+def checkout(request):
+    return HttpResponse("we are at checkout")
+
+
+
+
+
 
